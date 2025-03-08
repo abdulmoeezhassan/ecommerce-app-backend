4c36dc9863046e4e86df0443ee454dfe02274504
DevWhiz1 <ahmadbajwa3095@gmail.com>
2025-03-08 22:27:02 +0500

    cart,orders,products,customdesigns schema added cart and order api added

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const orderController = require("../controllers/orders.controller");
 const router = express.Router();
-
+//router
 router.post("/save", orderController.createOrder);
 router.get("/supplier/:id", orderController.getOrdersBySupplier);
 
